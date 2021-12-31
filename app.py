919a72b694e5a4ef09cb1c1306e3a25beec88c20
yukinaxx <iven1020310@gmail.com>
2022-01-01 01:09:49 +0800

    fixed instruction error

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ machine = TocMachine(
     show_conditions=True,
 )
 
-print("2022.1.1 01.05\n");
+print("2022.1.1 01.09\n");
 
 app = Flask(__name__, static_url_path="")
 
@@ -102,9 +102,9 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         #if machine.state == 'user':
-        send_text_massage(event.reply_token,'請問您要使用哪項功能？\n查詢今日日期(輸入「日期」)\n查詢某日星期(輸入「星期」)\n查詢現在時間(輸入「時間」)')
+        send_text_message(event.reply_token,'請問您要使用哪項功能？\n查詢今日日期(輸入「日期」)\n查詢某日星期(輸入「星期」)\n查詢現在時間(輸入「時間」)')
         if response == False:
-        	send_text_massage(event.reply_token,'請問您要使用哪項功能？\n查詢今日日期(輸入「日期」)\n查詢某日星期(輸入「星期」)\n查詢現在時間(輸入「時間」)')
+        	send_text_message(event.reply_token,'請問您要使用哪項功能？\n查詢今日日期(輸入「日期」)\n查詢某日星期(輸入「星期」)\n查詢現在時間(輸入「時間」)')
     return "OK"
 
 
