003cbb21867d1ebc136e1b9ded584e1ad29274b6
yukinaxx <iven1020310@gmail.com>
2021-12-31 09:20:57 +0800

    test demo code

--- fsm.py
+++ fsm.py
@@ -2,120 +2,35 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 
-import time
-import datetime
 
-print("3\n")
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
-        print("4\n")
 
-    def is_going_to_date(self, event):
+    def is_going_to_state1(self, event):
         text = event.message.text
-        flag = (text.lower() == "日期")
-        if flag == False:
-            self.go_back()
-        return flag
+        return text.lower() == "go to state1"
 
-    def is_going_to_weekday(self, event):
+    def is_going_to_state2(self, event):
         text = event.message.text
-        flag = (text.lower() == "星期")
-        if flag == False:
-            self.go_back()
-        return flag
+        return text.lower() == "go to state2"
 
-    def is_going_to_time(self, event):
-        text = event.message.text
-        flag = (text.lower() == "時間")
-        if flag == False:
-            self.go_back()
-        return flag
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
 
-    def on_enter_date(self):
-        send_text_message(reply_token, "請問您想知道西元日期或是民國日期？(輸入「西元」或「民國」)")
-
-    def on_enter_weekday(self):
-        send_text_message(reply_token, "請輸入想查詢的西元年月日(輸入「西元年/月/日」)")
-
-    def on_enter_time(self):
-        send_text_message(reply_token, "請問您想以12小時制還是24小時制表示？(輸入「12」或「24」)")
-
-    def is_going_to_date_AD(self, event):
-        text = event.message.text
-        flag = (text.lower() == "西元")
-        if flag == False:
-            self.go_back()
-        return flag
-
-    def is_going_to_date_ROC(self, event):
-        text = event.message.text
-        flag = (text.lower() == "民國")
-        if flag == False:
-            self.go_back()
-        return flag
-
-    def is_going_to_search(self, event):
-        global day
-        day = event.message.text
-        return True
-
-    def is_going_to_time_12(self, event):
-        text = event.message.text
-        flag = (text.lower() == "12")
-        if flag == False:
-            self.go_back()
-        return flag
-
-    def is_going_to_time_24(self, event):
-        text = event.message.text
-        flag = (text.lower() == "24")
-        if flag == False:
-            self.go_back()
-        return flag
-
-    def on_enter_date_AD(self):
-        now = datetime.datetime.now()
-        result = time.strftime("%Y-%m-%d", now)
-        print(result)
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state1")
         self.go_back()
-        return False
 
-    def on_enter_date_ROC(self):
-        now = datetime.datetime.now()
-        result = year(now()) - 1911 & mid(FormatDateTime(now(),2),5)
-        print(result)
+    def on_exit_state1(self):
+        print("Leaving state1")
+
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state2")
         self.go_back()
-        return False
 
-    def on_enter_search(self):
-        global day
-        day1 = day.split('/')
-        week_day_dict = {
-            0 : '星期一',
-            1 : '星期二',
-            2 : '星期三',
-            3 : '星期四',
-            4 : '星期五',
-            5 : '星期六',
-            6 : '星期天',
-        }
-        wday = datetime.datetime(day1[0],day[1],day[2]).strftime("%w")
-        send_text_message(reply_token, week_day_dict[wday])
-        self.go_back()
-        return False
-
-    def on_enter_time_12(self):
-        now = datetime.datetime.now()
-        result = time.strftime("%I:%M:%S %p", now)
-        print(result)
-        self.go_back()
-        return False
-
-    def on_enter_time_24(self):
-        now = datetime.datetime.now()
-        result = time.strftime("%H:%M:%S", now)
-        print(result)
-        self.go_back()
-        return False
-
+    def on_exit_state2(self):
+        print("Leaving state2")
